23742265b790bc26c01002f7f990d433b05ea233
Tolordava5 <georgetolordava5@gmail.com>
2025-05-14 08:47:48 +0400

    change obfuscator

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import obfuscator from "rollup-plugin-obfuscator"
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    obfuscator({
+      compact:true,
+      controlFlowFlattening:true,
+      deadCodeInjection:true,
+      rotateStringArray:true,
+      stringArrayEncoding:["base64"],
+      disableConsolOutput:true,
+    }),
     vueDevTools(),
   ],
   resolve: {
